99323c7da0df7643f03b17647e2240981d5c1af0
Claudio-Home <Cgsnascar@gmail.com>
2025-03-03 20:28:58 -0500

    added a footer to the page, updated site description

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import Header from './components/header';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import './styles/App.css';
-import DataDisplay from './components/DataDisplay';
+import React from "react";
+import { Helmet } from "react-helmet";
+import Header from "./components/header";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+import "./styles/App.css";
+import DataDisplay from "./components/DataDisplay";
 
 function App() {
   return (
     <div className="App">
       <Helmet>
         <title>Claudio Skala</title>
-        <meta name="description" content="Website portfolio for Claudio Skala" />
+        <meta
+          name="description"
+          content="Website portfolio for Claudio Skala"
+        />
         <meta property="og:title" content="Claudio Skala" />
-        <meta property="og:description" content="Website portfolio for Claudio Skala" />
+        <meta
+          property="og:description"
+          content="Website portfolio for Claudio Skala"
+        />
       </Helmet>
       <Header />
       <About />
       <Projects />
       <DataDisplay />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// footer for cgsnascar.dev portfolio website
+
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4">
+      <p className="text-center">© 2025 Claudio Skala. All rights reserved. Please contact the <a className='text-sky-400 hover:text-sky-700' href="mailto:webmaster@cgsnascar.dev">webmaster</a> if you have any issues with this website.</p>
+    </footer>
+  );
+}
+
+export default Footer;
